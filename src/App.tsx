2da0260c3d666bb65fdfb8ba786887986f1e3ed6
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-10-05 21:43:30 +0800

    nc

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Francis Deo Aurelio
         </p>
       </div>
       <p className="read-the-docs">
