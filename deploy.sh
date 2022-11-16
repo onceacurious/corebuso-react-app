1c12c55de761d11ad1448146bdafda15924e8fb7
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2022-11-16 06:31:57 +0000

    update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+!/usr/bin/env sh
 
 # abort on errors
 set -e
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:<onceacurious>/<corebuso-react-app>.git main:gh-pages
 
 cd -
