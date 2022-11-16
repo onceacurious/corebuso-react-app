d3bacf18c21cc060fe9e1f1e39862d921d3b915b
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2022-11-16 12:57:56 +0000

    nc

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'corebuso-react-app',
+  base: '/corebuso-react-app/',
   plugins: [react()]
 })
